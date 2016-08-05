7e1d0629a5b5ab8484fb425bf54e4f79c5ed480e
Tim Martin <tim@timmartin.me>
2016-08-05 11:40:15 -0400

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='thread-queue',
-    version='0.1.0',
+    version='0.2.0',
     description="A tool to queue tasks and execute them on a fixed number of threads",
     long_description=readme + '\n\n' + history,
     author="Tim Martin",
--- thread_queue.py
+++ thread_queue.py
@@ -12,7 +12,7 @@ except ImportError:
 
 __author__ = 'Tim Martin'
 __email__ = 'tim@timmartin.me'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 LOG = logging.getLogger(__name__)
 
